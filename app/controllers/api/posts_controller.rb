e888e0fbb97a84543d873f1913534ff90c41e6eb
Dirk Dobson <dirkvancouver@live.com>
2019-01-11 11:18:12 -0700

    posts controller actions

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,16 +1,42 @@
 class Api::PostsController < ApplicationController
+  before_action :set_post, :only [:show, :update, :destroy]
   def index
+    render json: current_user.Post.all
   end
 
   def show
+    render json: @post
   end
 
   def destroy
+    @post.destroy
   end
 
   def update
+    if @post.update(post_params)
+      render json: @post 
+    else
+      render json: post.errors, status: 422
+    end
   end
 
   def create
+    post = current_user.posts.new(post_params)
+
+    if post.save
+      render json: post
+    else
+      render json: post.errors, status: 422
+    end
+  end
+
+  private
+
+  def set_post
+    @post = current_user.post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:text, :likes, :dislikes)
   end
 end
