cc1b0f4586855fcd86416275b3a54e04bdfc3e0f
Dirk Dobson <dirkvancouver@live.com>
2019-01-11 15:15:11 -0700

    users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   
+  namespace :api do
+    get 'users/show'
+  end
   mount_devise_token_auth_for 'User', at: 'api/auth'
   namespace :api do
     resources :posts
