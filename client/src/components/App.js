5901eb75e3048f8e78c4544483374d777d066ad9
Jordan Rand <jorrand@gmail.com>
2019-01-11 12:50:30 -0700

    feed view component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,6 +9,7 @@ import Home from './Home';
 import ProtectedRoute from './ProtectedRoute';
 import AuthRoute from './AuthRoute';
 import FetchUser from './FetchUser';
+import Feed from './FeedView';
 
 class App extends Component {
   render() {
@@ -19,6 +20,7 @@ class App extends Component {
         <FetchUser>
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/feed" component={Feed} />
             <AuthRoute exact path="/login" component={Login} />
             <AuthRoute exact path="/register" component={Register} />
             <Route component={NoMatch} />
