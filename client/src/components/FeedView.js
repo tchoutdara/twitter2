3ebaf2d297de7db100385889bd5eb1ccbd43a8ed
Dirk Dobson <dirkvancouver@live.com>
2019-01-15 17:55:12 -0700

    route interpolation

--- client/src/components/FeedView.js
+++ client/src/components/FeedView.js
@@ -30,7 +30,7 @@ class FeedView extends React.Component {
   }
   
   addLike = (post) => {
-    axios.put('/api/posts/', post.id, {
+    axios.put(`/api/posts/${post.id}`, {
       likes: +1
     })
     .then(response => {
